aa1bb923d75c51b6954e3f03647a08f0d511f57f
Custromix <75646944+Custromix@users.noreply.github.com>
2026-04-15 00:22:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,21 +5,33 @@ import yt_dlp
 
 url = "https://www.youtube.com/watch?v=DdZluvojGWI"
 
-ydl_opts = {
-    "quiet": False,
+YTDL_OPTIONS = {
+    "format": "bestaudio/best",
+    "noplaylist": False,
+    "quiet": True,
     "listformats": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",
+    "verbose": True,
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android", "web"]
+        }
+    }
 }
+
 
 cookies_path = os.getenv("COOKIES_PATH")
 if cookies_path:
     if os.path.exists(cookies_path):
         print(f"✅ Fichier cookies trouvé : {cookies_path}")
-        ydl_opts["cookiefile"] = cookies_path
+        YTDL_OPTIONS["cookiefile"] = cookies_path
     else:
         print(f"❌ Fichier cookies introuvable : {cookies_path}")
 else:
     print("⚠️ COOKIES_PATH non défini dans le .env")
 
 #test
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+with yt_dlp.YoutubeDL(YTDL_OPTIONS) as ydl:
     ydl.extract_info(url, download=False)

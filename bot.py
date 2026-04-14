af65f1a047949a2c6c275be116e88adb5f9a522d
Custromix <75646944+Custromix@users.noreply.github.com>
2026-04-15 00:04:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 import yt_dlp
 
 url = "https://www.youtube.com/watch?v=DdZluvojGWI"
@@ -6,6 +9,16 @@ ydl_opts = {
     "quiet": False,
     "listformats": True,
 }
+
+cookies_path = os.getenv("COOKIES_PATH")
+if cookies_path:
+    if os.path.exists(cookies_path):
+        print(f"✅ Fichier cookies trouvé : {cookies_path}")
+        ydl_opts["cookiefile"] = cookies_path
+    else:
+        print(f"❌ Fichier cookies introuvable : {cookies_path}")
+else:
+    print("⚠️ COOKIES_PATH non défini dans le .env")
 
 #test
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
